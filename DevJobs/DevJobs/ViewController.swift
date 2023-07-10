5f874cf95e7add97ea0650c8862c34c77f26bdc8
Lucas Gomes <lgomeszx@gmail.com>
2023-07-10 19:45:37 -0300

    Criando projeto

---
+++ DevJobs/DevJobs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DevJobs
+//
+//  Created by Lucas Gomesx on 10/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
